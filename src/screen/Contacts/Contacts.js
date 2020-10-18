729e89074736337cc71562e8a531f144a29e3d3c
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-10-18 18:14:49 +0530

    Contact: Add screen design layout

--- src/screen/Contacts/Contacts.js
+++ src/screen/Contacts/Contacts.js
@@ -1,5 +1,52 @@
 import React from 'react';
+import {
+  Card,
+  CardItem,
+  Thumbnail,
+  List,
+  ListItem,
+  Left,
+  Text,
+  Input,
+  Item,
+} from 'native-base';
 
-const Contacts = () => <></>;
+import UserAvatar from '../../asset/image/user.png';
+import { ContactDetails, ContactsContainer } from './Contacts.style';
+import { COLOR } from '../../constant';
+
+const Contacts = () => {
+  return (
+    <ContactsContainer>
+      <Card>
+        <CardItem header>
+          <Item regular rounded style={{ borderColor: COLOR.PRIMARY }}>
+            <Input placeholder="Input" />
+          </Item>
+        </CardItem>
+        <List>
+          <ListItem avatar>
+            <Left>
+              <Thumbnail small source={UserAvatar} />
+            </Left>
+            <ContactDetails>
+              <Text>Praveen</Text>
+              <Text note>9876543210</Text>
+            </ContactDetails>
+          </ListItem>
+          <ListItem avatar>
+            <Left>
+              <Thumbnail small source={UserAvatar} />
+            </Left>
+            <ContactDetails>
+              <Text>Praveen</Text>
+              <Text note>9876543210</Text>
+            </ContactDetails>
+          </ListItem>
+        </List>
+      </Card>
+    </ContactsContainer>
+  );
+};
 
 export default Contacts;
---
+++ src/screen/Contacts/Contacts.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import { Body } from 'native-base';
+
+import { SPACING } from '../../constant';
+
+export const ContactsContainer = styled.View`
+  flex: 1;
+  padding-horizontal: ${SPACING.SPACE_20};
+`;
+
+export const ContactDetails = styled(Body)`
+  border-bottom-width: 0;
+`;
