6629639c5cd150292cdc7c50f0a962e636795a90
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-11-09 18:26:27 +0530

    Navigation: Remove background component in navigation config

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Background1 } from '../component/Background';
-import { ROUTE, COLOR, CONSTANT } from '../constant';
+import { ROUTE, COLOR } from '../constant';
 import Login from '../screen/Login';
 import Signin from '../screen/Signin';
 import Signup from '../screen/Signup';
@@ -19,24 +17,8 @@ export default () => (
       cardStyle: { backgroundColor: COLOR.BACKGROUND },
     }}>
     <Stack.Screen name={ROUTE.LOGIN} component={Login} />
-    <Stack.Screen
-      name={ROUTE.SIGNIN}
-      component={Signin}
-      options={{
-        cardOverlayEnabled: true,
-        cardOverlay: () =>
-          Platform.OS === CONSTANT.OS.ANDROID && <Background1 />,
-      }}
-    />
-    <Stack.Screen
-      name={ROUTE.SIGNUP}
-      component={Signup}
-      options={{
-        cardOverlayEnabled: true,
-        cardOverlay: () =>
-          Platform.OS === CONSTANT.OS.ANDROID && <Background1 />,
-      }}
-    />
+    <Stack.Screen name={ROUTE.SIGNIN} component={Signin} />
+    <Stack.Screen name={ROUTE.SIGNUP} component={Signup} />
     <Stack.Screen name={ROUTE.HOME} component={HomeBottomTabNavigator} />
   </Stack.Navigator>
 );
