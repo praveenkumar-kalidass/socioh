6ec9fe5acbd7ba52ba4b371ab985beec6e043b8d
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-11-11 13:46:03 +0530

    Contact: Add background pattern for screen

--- src/screen/Contact/Contact.js
+++ src/screen/Contact/Contact.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { CONSTANT } from '../../constant';
+import wrapPattern from '../../helper/wrapPattern';
+
 const Contact = () => <></>;
 
-export default Contact;
+export default wrapPattern({
+  Component: Contact,
+  pattern: CONSTANT.SCREEN_BACKGROUND.BACKGROUND_2,
+});
