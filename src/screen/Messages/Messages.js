5b68afb5643416c2b43ff187f0d4272b77679748
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-12-18 15:22:12 +0530

    Messages: Add navigation on click of message card

--- src/screen/Messages/Messages.js
+++ src/screen/Messages/Messages.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MessageCard from '../../component/MessageCard';
 import SociohBot from '../../asset/image/socioh_bot.jpg';
-import { CONSTANT } from '../../constant';
+import { CONSTANT, ROUTE } from '../../constant';
 import { MessagesContainer } from './Messages.style';
 
-const Messages = () => {
+const Messages = ({ navigation }) => {
+  const navigateToMessage = (data) => {
+    navigation.navigate(ROUTE.MESSAGE, { ...data, title: data.name });
+  };
+
   return (
     <MessagesContainer>
-      <MessageCard highlight image={SociohBot} primaryText={CONSTANT.APP_BOT} />
+      <MessageCard
+        highlight
+        image={SociohBot}
+        primaryText={CONSTANT.APP_BOT}
+        onPress={() =>
+          navigateToMessage({ image: SociohBot, name: CONSTANT.APP_BOT })
+        }
+        testID="socioh_bot"
+      />
     </MessagesContainer>
   );
+};
+
+Messages.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
 };
 
 export default Messages;
--- src/screen/Messages/Messages.test.js
+++ src/screen/Messages/Messages.test.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 import Messages from './index';
 
 describe('Messages', () => {
+  const mockNavigation = { navigate: jest.fn() };
+
   it('should match snapshot', () => {
-    const container = render(<Messages />);
+    const container = render(<Messages navigation={mockNavigation} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('should navigate to Message screen with data in route params', () => {
+    const { getByTestId } = render(<Messages navigation={mockNavigation} />);
+
+    fireEvent.press(getByTestId('socioh_bot'));
+
+    expect(mockNavigation.navigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigation.navigate).toHaveBeenCalledWith(
+      'MESSAGE',
+      expect.objectContaining({
+        name: 'Socioh Bot',
+        title: 'Socioh Bot',
+      }),
+    );
   });
 });
