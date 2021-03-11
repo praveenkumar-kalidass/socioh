0765be879242e8b94dc36c08c88c636800d50238
praveenkumar-kalidass <praveen.ece@outlook.com>
2021-03-11 20:25:01 +0530

    Photos: Add screen layout

--- src/screen/Photos/Photos.js
+++ src/screen/Photos/Photos.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Card, CardItem } from 'native-base';
 
-const Photos = () => <></>;
+import Praveen from '../../asset/image/praveen.jpeg';
+import User from '../../asset/image/user.png';
+import Bot from '../../asset/image/socioh_bot.jpg';
+import {
+  Gallery,
+  GalleryGrid,
+  GalleryItem,
+  PhotosContainer,
+} from './Photos.style';
+
+const Photos = () => {
+  return (
+    <PhotosContainer>
+      <Card>
+        <CardItem cardBody>
+          <Gallery>
+            <GalleryGrid>
+              <GalleryItem source={Praveen} />
+            </GalleryGrid>
+            <GalleryGrid>
+              <GalleryItem source={User} />
+            </GalleryGrid>
+            <GalleryGrid>
+              <GalleryItem source={Bot} />
+            </GalleryGrid>
+            <GalleryGrid>
+              <GalleryItem source={Praveen} />
+            </GalleryGrid>
+          </Gallery>
+        </CardItem>
+      </Card>
+    </PhotosContainer>
+  );
+};
 
 export default Photos;
---
+++ src/screen/Photos/Photos.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+import { SPACING } from '../../constant';
+
+export const PhotosContainer = styled.View`
+  flex: 1;
+  padding-horizontal: ${SPACING.SPACE_20};
+`;
+
+export const Gallery = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: ${SPACING.SPACE_20};
+`;
+
+export const GalleryGrid = styled.View`
+  width: 30%;
+  margin-vertical: ${SPACING.SPACE_10};
+`;
+
+export const GalleryItem = styled.Image`
+  aspect-ratio: 1;
+  height: auto;
+  width: auto;
+`;
