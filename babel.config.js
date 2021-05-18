c9d9743192462bf14d3488ff0355c18a54002043
praveenkumar-kalidass <praveen.ece@outlook.com>
2021-05-18 20:21:04 +0530

    Package: Configure module resolver babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['jsx-control-statements'],
+  plugins: [
+    'jsx-control-statements',
+    [
+      'module-resolver',
+      {
+        root: './src',
+        alias: {
+          asset: './src/asset',
+          component: './src/component',
+          constant: './src/constant',
+          helper: './src/helper',
+          hook: './src/hook',
+          navigation: './src/navigation',
+          screen: './src/screen',
+          translation: './src/translation',
+        },
+        transformFunctions: ['jest.mock'],
+      },
+    ],
+  ],
 };
